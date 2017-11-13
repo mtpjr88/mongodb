cc4af3013c3d126a5cf42480309aeb31f0542d64
Michael Pimentel <mtpjr88@gmail.com>
2017-11-12 16:27:11 -0800

    new routes for GET and POST

--- server/server.js
+++ server/server.js
@@ -1,10 +1,14 @@
+//(run in terminal to start mongo server) ./mongod --dbpath ~/mongo-data
+
 var express = require('express');
 var bodyParser = require('body-parser');
+var { ObjectID } = require('mongodb');
 
 
-var {mongoose} = require('./db/mongoose');
-var {Todo} = require('./models/todo');
-var {User} = require('./models/user');
+var { mongoose } = require('./db/mongoose');
+var { Todo } = require('./models/todo');
+var { User } = require('./models/user');
+let { Lamp } = require('./models/lamp');
 
 var app = express();
 
@@ -27,7 +31,24 @@ app.post("/todos", (req,res) => {
     });
 });
 
-//Get route returns get requests
+app.post("/lamps", (req, res) => {
+     var lamp = new Lamp({
+        size: req.body.sizes,
+        date: req.body.date,
+        outsideDiameter: req.body.outsideDiameter,
+        overallLength: req.body.overallLength,
+        arcGap: req.body.arcGap,
+        comments: req.body.comments
+     });
+
+     lamp.save().then((doc) => {
+         res.send(doc);
+     }, (err) => {
+        res.status(400).send(err);
+     });
+});
+
+//GET route returns get requests
 app.get('/todos', (req, res) => {
     Todo.find().then((todos) => {
         res.send({todos});
@@ -35,6 +56,27 @@ app.get('/todos', (req, res) => {
         res.status(400).send(err);
     });
 });
+
+//GET route for lamps
+
+app.get('/lamps', (req, res) => {
+    Lamp.find().then( (lamps) => {
+        res.send({lamps});
+    },(err) => res.status(400).send(err));
+});
+
+// GET /todos/12341234
+app.get('/todos/:id', (req, res)=> {
+    let id = req.params.id;
+
+    if(!ObjectID.isValid(id)) console.log(`ID not valid`);
+
+    Todo.findById(id).then((todo) => {
+        res.send({todo});
+    }).catch((err) => res.status(404).send(err));
+    
+});
+
 
 app.listen(3000,() => {
     console.log("Started on Port 3000");
